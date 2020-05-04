2aa2a75791a4e432c100bc09e7ba8140d5d1b251
Frank <mingyi.frank@gmail.com>
2020-05-04 22:56:17 +0800

    updates

--- Core/src/com/icenter/core/client/rest/convert/JSONConverter.java
+++ Core/src/com/icenter/core/client/rest/convert/JSONConverter.java
@@ -6,7 +6,7 @@ import com.icenter.core.client.lambda.Function;
 public abstract class JSONConverter<T> {
 
     public final static String Name = JSONConverter.class.getName();
-    private Function<String, String> nameFormatter = name -> name;
+    private Function<String, String> paramNameFormatter = name -> name;
     private JSONProperty property;
 
     public JSONConverter(){ }
@@ -14,8 +14,8 @@ public abstract class JSONConverter<T> {
     public abstract JSONValue convertObjectToJSON(T object);
     public abstract T convertJSONToObject(JSONValue value);
 
-    public final void setNameFormatter(Function<String,String> nameFormatter) {
-        this.nameFormatter = nameFormatter;
+    public final void setParamNameFormatter(Function<String,String> nameFormatter) {
+        this.paramNameFormatter = nameFormatter;
     }
 
     public final void setProperty(JSONProperty property) {
