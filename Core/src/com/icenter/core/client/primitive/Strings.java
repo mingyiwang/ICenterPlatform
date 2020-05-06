45c0fab28958d270ee0dbaf888066d9a61bb6339
Frank <mingyi.frank@gmail.com>
2020-05-06 10:01:56 +0800

    updates

---
+++ Core/src/com/icenter/core/client/primitive/Strings.java
@@ -0,0 +1,11 @@
+package com.icenter.core.client.primitive;
+
+public final class Strings {
+
+    public static String Empty = "";
+    public static String WhiteSpace_1 = " ";
+    public static String DOT = ".";
+    public static String Comma = ",";
+
+
+}
